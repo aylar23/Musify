636816b88e59d875cfa64dc472766b857e820aa8
Yusup Aydogdyyev <aydogdyevyusup@gmail.com>
2023-09-20 20:13:21 +0500

    added bottom sheet

--- app/src/main/java/com/musify/app/presentation/common/MusicSettingsView.kt
+++ app/src/main/java/com/musify/app/presentation/common/MusicSettingsView.kt
@@ -1,94 +1,53 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.musify.app.presentation.common
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.musify.app.R
-import com.musify.app.domain.models.Song
 import com.musify.app.ui.theme.AlbumCoverBlackBG
 import com.musify.app.ui.theme.SFFontFamily
 import com.musify.app.ui.theme.WhiteTextColor
 
+
 @Composable
-fun MusicSettingsView(song: Song?, settingsClicked: MutableState<Boolean>, hide: () -> Unit) {
+fun bottomSheet(settingsClicked: MutableState<Boolean>, onDismiss: () -> Unit) {
+    val sheetState = rememberModalBottomSheetState()
 
-    AnimatedVisibility(
-        visible = settingsClicked.value,
-        enter = slideInVertically(
-            initialOffsetY = { it/2 },
-            animationSpec = tween(100)
-        ),
-        exit = slideOutVertically(
-            targetOffsetY = {
-                it / 2
-            },
-            animationSpec = tween(100)
-        )
-    ) {
-        Box(modifier = Modifier
-            .fillMaxSize()){
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .alpha(.3f)
-                .background(AlbumCoverBlackBG)
-                .clickable { hide() }
-            )
-            Column(modifier = Modifier
-                .clip(shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                .background(AlbumCoverBlackBG)
-                .align(Alignment.BottomCenter)) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center) {
-                    Spacer(modifier = Modifier.weight(1.2f))
-                    HorizontalDivider(
-                        modifier = Modifier.weight(.2f),
-                        thickness = 1.dp,
-                        color = WhiteTextColor)
-                    Spacer(modifier = Modifier.weight(1.2f))
-                }
-                ActionsModelView(expandable = true, icon = R.drawable.library_add, text = "Pleyliste Gos")
-                ActionsModelView(expandable = false, icon = R.drawable.redo, text = "Indiki Edip Cal")
-                ActionsModelView(expandable = true, icon = R.drawable.account_circle, text = "Bagshyny Gor")
-                ActionsModelView(expandable = true, icon = R.drawable.album, text = "Albomy gor")
-                ActionsModelView(expandable = false, icon = R.drawable.share, text = "Paylashmak")
-            }
 
+    if (settingsClicked.value){
+        ModalBottomSheet(
+            containerColor = AlbumCoverBlackBG,
+            sheetState = sheetState,
+            onDismissRequest = { onDismiss }) {
+            ActionsModelView(expandable = true, icon = R.drawable.library_add, text = "Pleyliste Gos")
+            ActionsModelView(expandable = false, icon = R.drawable.redo, text = "Indiki Edip Cal")
+            ActionsModelView(expandable = true, icon = R.drawable.account_circle, text = "Bagshyny Gor")
+            ActionsModelView(expandable = true, icon = R.drawable.album, text = "Albomy gor")
+            ActionsModelView(expandable = false, icon = R.drawable.share, text = "Paylashmak")
         }
     }
-
 }
 
 
+
 @Composable
-private fun ActionsModelView(icon: Int, text: String, expandable: Boolean){
+fun ActionsModelView(icon: Int, text: String, expandable: Boolean){
     Row(modifier = Modifier.padding(14.dp),
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically) {
--- app/src/main/java/com/musify/app/presentation/topdetails/TopDetailsScreen.kt
+++ app/src/main/java/com/musify/app/presentation/topdetails/TopDetailsScreen.kt
@@ -18,10 +18,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -38,8 +40,8 @@ import androidx.compose.ui.unit.dp
 import com.musify.app.R
 import com.musify.app.domain.models.Song
 import com.musify.app.domain.models.defaultSong
+import com.musify.app.presentation.common.ActionsModelView
 import com.musify.app.presentation.common.CustomButton
-import com.musify.app.presentation.common.MusicSettingsView
 import com.musify.app.presentation.common.SongView
 import com.musify.app.presentation.topdetails.components.CollapsingTopAppBar
 import com.musify.app.ui.theme.AlbumCoverBlackBG
@@ -163,19 +165,38 @@ fun TopDetailsScreen(paddingValues: PaddingValues) {
                 SongView(song = defaultSong) {
                     settingsClicked.value = true
                     selectedSong = defaultSong
-                    Log.i("selectSongCheck", "${settingsClicked.value}")
-
+                    Log.i("settingsClicked", "true")
                 }
             }
 
         }
 
-        MusicSettingsView(song = selectedSong, settingsClicked) {
-            settingsClicked.value = !settingsClicked.value
+        // Shular yaly edip bashga moduldan chagyramda composably bir gezek yasaya
+        // onnon hemme zat donya, sebabi bottomSheet bir gezek yasandan son ayrylanok shol composable
+        // ekrany tutup durya oydyan hic knopka click edilenni bilenok sebabi ustlerinde bir composable bar
+//        bottomSheet(settingsClicked) {
+//            settingsClicked.value = false
+//        }
+
+        val sheetState = rememberModalBottomSheetState()
+
+        // Shon uchin sheydip her screenda tazeden yazmaly bolyas sheydayelimi?
+        if (settingsClicked.value){
+            ModalBottomSheet(
+                containerColor = AlbumCoverBlackBG,
+                sheetState = sheetState,
+                onDismissRequest = { settingsClicked.value = false }) {
+                ActionsModelView(expandable = true, icon = R.drawable.library_add, text = "Pleyliste Gos")
+                ActionsModelView(expandable = false, icon = R.drawable.redo, text = "Indiki Edip Cal")
+                ActionsModelView(expandable = true, icon = R.drawable.account_circle, text = "Bagshyny Gor")
+                ActionsModelView(expandable = true, icon = R.drawable.album, text = "Albomy gor")
+                ActionsModelView(expandable = false, icon = R.drawable.share, text = "Paylashmak")
+            }
         }
 
-
     }
+
+
 
 
 }
